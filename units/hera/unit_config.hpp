a5d40681a09fa153a031ac9add34a2f8d2bf7c3b
Soo-Hyun Yoo <yoos117@gmail.com>
2014-10-31 03:21:43 -0700

    refs #5 Add hera files.
    Forgot last time, I guess.

---
+++ units/hera/unit_config.hpp
@@ -0,0 +1,5 @@
+// Maximum angular position in the roll and pitch axes (deg)
+static const float MAX_PITCH_ROLL_POS = 30.0;
+
+// Maximum angular velocity in the roll and pitch axes (deg/s)
+static const float MAX_PITCH_ROLL_VEL = 100.0;
