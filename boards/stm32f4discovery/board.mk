35402e57ec8b7a11d6e9a6b1fbc59ba29ba12eac
Soo-Hyun Yoo <yoos117@gmail.com>
2014-10-30 20:15:48 -0700

    Link discovery board to F407, not F405.

--- boards/stm32f4discovery/board.mk
+++ boards/stm32f4discovery/board.mk
@@ -4,7 +4,7 @@ include $(CHIBIOS)/os/hal/platforms/STM32F4xx/platform.mk
 include $(CHIBIOS)/os/ports/GCC/ARMCMx/STM32F4xx/port.mk
 
 # Define linker script file
-LDSCRIPT= $(PORTLD)/STM32F405xG.ld
+LDSCRIPT= $(PORTLD)/STM32F407xG.ld
 
 # Include directory for mcuconf.h
 CONFINC = boards/stm32f4discovery
