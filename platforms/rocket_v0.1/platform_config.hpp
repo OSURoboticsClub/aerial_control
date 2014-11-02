aaf531c733f15a6a94856b2c96343b70cb5f261e
Soo-Hyun Yoo <yoos117@gmail.com>
2014-11-02 00:20:02 -0700

    refs #5 Add rocket source files!

---
+++ platforms/rocket_v0.1/platform_config.hpp
@@ -0,0 +1,19 @@
+#ifndef PLATFORM_CONFIG_HPP_
+#define PLATFORM_CONFIG_HPP_
+
+// Drivers
+#include <drivers/l3gd20.hpp>
+#include <drivers/lsm303dlhc.hpp>
+
+// Systems
+#include <system/default_multirotor_vehicle_system.hpp>
+
+namespace platform {
+
+extern L3GD20 gyro;
+extern LSM303DLHC accel;
+extern DefaultMultirotorVehicleSystem system;
+
+}
+
+#endif // PLATFORM_CONFIG_HPP_
---
+++ platforms/rocket_v0.1/usart_config.cpp
@@ -0,0 +1,11 @@
+#include <hal.h>
+
+#include <usart_config.hpp>
+
+SerialDriver *usartPlatformInit(void) {
+  sdStart(&SD1, &usart1_config);
+  palSetPadMode(GPIOA, 9, PAL_MODE_ALTERNATE(7));
+  palSetPadMode(GPIOA, 10, PAL_MODE_ALTERNATE(7));
+  return &SD1;
+}
+
