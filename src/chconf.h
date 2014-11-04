022f7b202a1be300da492f98aadc6fab751cfcfe
Kyle Cesare <kcesare@gmail.com>
2014-11-03 18:14:38 -0800

    Lower CH_FREQUENCY to 250000.

--- src/chconf.h
+++ src/chconf.h
@@ -41,7 +41,7 @@
  *          setting also defines the system tick time unit.
  */
 #if !defined(CH_FREQUENCY) || defined(__DOXYGEN__)
-#define CH_FREQUENCY                    500000
+#define CH_FREQUENCY                    200000
 #endif
 
 /**
