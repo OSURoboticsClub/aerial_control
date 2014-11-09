d70dfc2fff37b32ddcf4df9c006bbc7f83323dd4
Soo-Hyun Yoo <yoos117@gmail.com>
2014-11-08 23:58:46 -0800

    Add sensor offsets to units.

--- units/apollo/unit_config.hpp
+++ units/apollo/unit_config.hpp
@@ -3,3 +3,11 @@ static const float MAX_PITCH_ROLL_POS = 30.0;
 
 // Maximum angular velocity in the roll and pitch axes (deg/s)
 static const float MAX_PITCH_ROLL_VEL = 100.0;
+
+// Sensor offsets
+const float GYR_X_OFFSET = 0.0;
+const float GYR_Y_OFFSET = 0.0;
+const float GYR_Z_OFFSET = 0.0;
+const float ACC_X_OFFSET = 0.0;
+const float ACC_Y_OFFSET = 0.0;
+const float ACC_Z_OFFSET = 0.0;
--- units/hera/unit_config.hpp
+++ units/hera/unit_config.hpp
@@ -1,5 +1,13 @@
 // Maximum angular position in the roll and pitch axes (deg)
-static const float MAX_PITCH_ROLL_POS = 30.0;
+const float MAX_PITCH_ROLL_POS = 30.0;
 
 // Maximum angular velocity in the roll and pitch axes (deg/s)
-static const float MAX_PITCH_ROLL_VEL = 100.0;
+const float MAX_PITCH_ROLL_VEL = 100.0;
+
+// Sensor offsets
+const float GYR_X_OFFSET = 0.0;
+const float GYR_Y_OFFSET = 0.0;
+const float GYR_Z_OFFSET = 0.0;
+const float ACC_X_OFFSET = 0.0;
+const float ACC_Y_OFFSET = 0.0;
+const float ACC_Z_OFFSET = 0.0;
