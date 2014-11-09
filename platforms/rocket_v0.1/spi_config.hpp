dcc18d1c21aeed09374306ad5457e5da1bd38bec
Soo-Hyun Yoo <yoos117@gmail.com>
2014-11-08 23:56:59 -0800

    Set up MPU-6000 SPI config for rocket.

--- platforms/rocket_v0.1/spi_config.hpp
+++ platforms/rocket_v0.1/spi_config.hpp
@@ -1,11 +1,14 @@
 #ifndef SPI_CONFIG_HPP_
 #define SPI_CONFIG_HPP_
 
-// SPI configuration
-const SPIConfig spi1_config = {
+// Mutex to lock output buffer
+static Mutex spi_mtx;
+
+// MPU6000 SPI configuration
+const SPIConfig mpu6000_spicfg = {
   NULL,
-  GPIOB,
-  2,
+  GPIOC,
+  14,
   SPI_CR1_BR_0   // 42000000/2^1 = 21000000
 };
 
