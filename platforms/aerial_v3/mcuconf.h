43e88cde2320fe2bcdacdc8ba01d737c63e80e1d
Soo-Hyun Yoo <yoos117@gmail.com>
2014-11-10 01:24:12 -0800

    Disable TIM1 and TIM3 for aerial_v3.

--- platforms/aerial_v3/mcuconf.h
+++ platforms/aerial_v3/mcuconf.h
@@ -188,9 +188,9 @@
  * PWM driver system settings.
  */
 #define STM32_PWM_USE_ADVANCED              FALSE
-#define STM32_PWM_USE_TIM1                  TRUE
+#define STM32_PWM_USE_TIM1                  FALSE
 #define STM32_PWM_USE_TIM2                  FALSE
-#define STM32_PWM_USE_TIM3                  TRUE
+#define STM32_PWM_USE_TIM3                  FALSE
 #define STM32_PWM_USE_TIM4                  FALSE
 #define STM32_PWM_USE_TIM5                  FALSE
 #define STM32_PWM_USE_TIM8                  TRUE
