fa5a7d296a84c5e1dc5acd1d865d6ad61e1f24c7
Soo-Hyun Yoo <yoos117@gmail.com>
2014-11-10 01:54:23 -0800

    Enable USART3 for aerial_v3.

--- platforms/aerial_v3/usart_config.cpp
+++ platforms/aerial_v3/usart_config.cpp
@@ -4,7 +4,10 @@
 
 void usartPlatformInit(void) {
   sdStart(&SD1, &usart1_config);
-  palSetPadMode(GPIOA, 9, PAL_MODE_ALTERNATE(7));
-  palSetPadMode(GPIOA, 10, PAL_MODE_ALTERNATE(7));
+  sdStart(&SD3, &usart3_config);
+  palSetPadMode(GPIOB, 6, PAL_MODE_ALTERNATE(7));   // USART1 TX
+  palSetPadMode(GPIOB, 7, PAL_MODE_ALTERNATE(7));   // USART1 RX
+  palSetPadMode(GPIOB, 10, PAL_MODE_ALTERNATE(7));   // USART3 TX
+  palSetPadMode(GPIOB, 11, PAL_MODE_ALTERNATE(7));   // USART RX
 }
 
