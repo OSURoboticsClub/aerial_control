2327581eb09c9c787fde59987ab822d385d3ffa0
Soo-Hyun Yoo <yoos117@gmail.com>
2014-11-13 14:26:26 -0800

    Use SPI exchange function.

--- src/drivers/mpu6000.cpp
+++ src/drivers/mpu6000.cpp
@@ -1,6 +1,7 @@
 #include <drivers/mpu6000.hpp>
 
 #include <hal.h>
+#include <chprintf.h>
 
 #include <hal_config.hpp>
 
@@ -10,53 +11,50 @@ MPU6000::MPU6000(SPIDriver *spid, const SPIConfig *spicfg) : spid(spid), spicfg(
 void MPU6000::init() {
   uint8_t txbuf[8], rxbuf[8];
 
-  // Start bus
-  //chMtxLock(&spi_mtx);
-  spiAcquireBus(spid);      // Acquire bus ownership
-  spiStart(spid, spicfg);   // Set up transfer parameters
-  spiSelect(spid);          // Assert slave select
-
   // Reset device.
   txbuf[0] = MPU6000_PWR_MGMT_1;
   txbuf[1] = 0x80;
-  spiExchange(spid, 2, txbuf, rxbuf);   // Exchange data.
+  _spiExchange(spid, spicfg, 2, txbuf, rxbuf);   // Exchange data.
 
-  chThdSleepMilliseconds(100);
+  chThdSleepMilliseconds(100);   // TODO(yoos): Check whether or not datasheet specifies this holdoff.
 
   // Set sample rate to 1 kHz.
   txbuf[0] = MPU6000_SMPLRT_DIV;
   txbuf[1] = 0x00;
-  spiExchange(spid, 2, txbuf, rxbuf);   // Exchange data.
+  _spiExchange(spid, spicfg, 2, txbuf, rxbuf);   // Exchange data.
 
   // Set DLPF to 4 (20 Hz gyro bandwidth1 Hz accelerometer bandwidth)
   txbuf[0] = MPU6000_CONFIG;
   txbuf[1] = 0x04;
-  spiExchange(spid, 2, txbuf, rxbuf);   // Exchange data.
+  _spiExchange(spid, spicfg, 2, txbuf, rxbuf);   // Exchange data.
 
   // Wake up device and set clock source to Gyro Z.
   txbuf[0] = MPU6000_PWR_MGMT_1;
   txbuf[1] = 0x03;
-  spiExchange(spid, 2, txbuf, rxbuf);   // Exchange data.
+  _spiExchange(spid, spicfg, 2, txbuf, rxbuf);   // Exchange data.
 
   // Set gyro full range to 2000 dps. We first read in the current register
   // value so we can change what we need and leave everything else alone.
   txbuf[0] = MPU6000_GYRO_CONFIG | (1<<7);
-  spiExchange(spid, 2, txbuf, rxbuf);   // Exchange data.
+  _spiExchange(spid, spicfg, 2, txbuf, rxbuf);   // Exchange data.
   txbuf[0] = MPU6000_GYRO_CONFIG;
   txbuf[1] = (rxbuf[1] & ~0x18) | 0x18;
-  spiExchange(spid, 2, txbuf, rxbuf);   // Exchange data.
+  _spiExchange(spid, spicfg, 2, txbuf, rxbuf);   // Exchange data.
 
   // Set accelerometer full range to 2 g.
   txbuf[0] = MPU6000_ACCEL_CONFIG | (1<<7);
-  spiExchange(spid, 2, txbuf, rxbuf);   // Exchange data.
+  _spiExchange(spid, spicfg, 2, txbuf, rxbuf);   // Exchange data.
   txbuf[0] = MPU6000_ACCEL_CONFIG;
   txbuf[1] = (rxbuf[1] & ~0x18) | 0x00;
-  spiExchange(spid, 2, txbuf, rxbuf);   // Exchange data.
+  _spiExchange(spid, spicfg, 2, txbuf, rxbuf);   // Exchange data.
 
-  // Stop bus
-  spiUnselect(spid);     // Deassert slave select
-  spiReleaseBus(spid);   // Release bus ownership
-  //chMtxUnlock();
+  // DEBUG
+  txbuf[0] = MPU6000_TEMP_OUT_H | (1<<7);
+  _spiExchange(spid, spicfg, 3, txbuf, rxbuf);   // Exchange data.
+
+  char dbg_buf[8];
+  chsnprintf(dbg_buf, 3, "%02x", rxbuf[2]);
+  chnWriteTimeout((BaseChannel*)&SD3, (uint8_t*)dbg_buf, 3, MS2ST(20));
 
   // Read once to clear out bad data?
   readGyro();
@@ -67,28 +65,20 @@ gyroscope_reading_t MPU6000::readGyro() {
   uint8_t txbuf[8], rxbuf[8];
   gyroscope_reading_t reading;
 
-  // Start bus
-  //chMtxLock(&spi_mtx);
-  spiAcquireBus(spid);      // Acquire bus ownership
-  spiStart(spid, spicfg);   // Set up transfer parameters
-  spiSelect(spid);          // Assert slave select
-
-  // Get data
+  // Poll gyro
   txbuf[0] = MPU6000_GYRO_XOUT_H | (1<<7);
-  spiExchange(spid, 7, txbuf, rxbuf);   // Atomic transfer operations
+  _spiExchange(spid, spicfg, 7, txbuf, rxbuf);   // Atomic transfer operations
+
+  // TODO(yoos): correct for thermal bias.
   reading.axes[0] = ((int16_t) ((rxbuf[1]<<8) | rxbuf[2])) / 16.384 * 3.1415926535 / 180.0 + GYR_X_OFFSET;
   reading.axes[1] = ((int16_t) ((rxbuf[3]<<8) | rxbuf[4])) / 16.384 * 3.1415926535 / 180.0 + GYR_Y_OFFSET;
   reading.axes[2] = ((int16_t) ((rxbuf[5]<<8) | rxbuf[6])) / 16.384 * 3.1415926535 / 180.0 + GYR_Z_OFFSET;
 
-  // TODO(yoos): correct for thermal bias.
+  // Poll temp
   txbuf[0] = MPU6000_TEMP_OUT_H | (1<<7);
-  spiExchange(spid, 3, txbuf, rxbuf);
-  float temp = ((int16_t) ((rxbuf[1]<<8) | rxbuf[2])) / 340 + 36.53;
+  _spiExchange(spid, spicfg, 3, txbuf, rxbuf);
 
-  // Stop bus
-  spiUnselect(spid);     // Deassert slave select
-  spiReleaseBus(spid);   // Release bus ownership
-  //chMtxUnlock();
+  float temp = ((int16_t) ((rxbuf[1]<<8) | rxbuf[2])) / 340 + 36.53;
 
   return reading;
 }
@@ -97,23 +87,12 @@ accelerometer_reading_t MPU6000::readAccel() {
   uint8_t txbuf[8], rxbuf[8];
   accelerometer_reading_t reading;
 
-  // Start bus
-  //chMtxLock(&spi_mtx);
-  spiAcquireBus(spid);      // Acquire bus ownership
-  spiStart(spid, spicfg);   // Set up transfer parameters
-  spiSelect(spid);          // Assert slave select
-
   // Get data
   txbuf[0] = MPU6000_ACCEL_XOUT_H | (1<<7);
-  spiExchange(spid, 7, txbuf, rxbuf);   // Atomic transfer operations
+  _spiExchange(spid, spicfg, 7, txbuf, rxbuf);   // Atomic transfer operations
   reading.axes[0] = ((int16_t) ((rxbuf[1]<<8) | rxbuf[2])) / 16384.0 + ACC_X_OFFSET;
   reading.axes[1] = ((int16_t) ((rxbuf[3]<<8) | rxbuf[4])) / 16384.0 + ACC_Y_OFFSET;
   reading.axes[2] = ((int16_t) ((rxbuf[5]<<8) | rxbuf[6])) / 16384.0 + ACC_Z_OFFSET;
-
-  // Stop bus
-  spiUnselect(spid);     // Deassert slave select
-  spiReleaseBus(spid);   // Release bus ownership
-  //chMtxUnlock();
 
   return reading;
 }
