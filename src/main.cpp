492a6374de096a690348adb70063b0da9fd2605b
Kyle Cesare <kcesare@gmail.com>
2014-12-16 22:48:52 -0800

    Send heartbeat messages.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 #include <ch.hpp>
 #include <hal.h>
 
+#include <array> // TODO(kyle):
+#include <protocol.hpp>
+#include <messages.hpp>
+#include <encoder.hpp>
+#include <decoder.hpp>
+
 #include <hal_config.hpp>
 #include <platform_config.hpp>
 
@@ -9,6 +15,18 @@
 
 static platform::HeartbeatThread heartbeatThread;
 static Debugger debugger;
+
+void communicate() {
+  protocol::Encoder encoder;
+  protocol::message::heartbeat_message_t message {
+    .seq = 0x50
+  };
+
+  std::array<std::uint8_t, 3> buffer;
+  encoder.encode(message, &buffer);
+
+  chnWrite((BaseSequentialStream *) &SD1, buffer.data(), 3);
+}
 
 int main(void) {
   halInit();
@@ -28,6 +46,7 @@ int main(void) {
     deadline += MS2ST(DT * 1000);
 
     platform::system.update();
+    communicate();
 
     chibios_rt::BaseThread::sleepUntil(deadline);
   }
