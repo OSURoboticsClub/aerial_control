a2aa734678491f0587e23d0d71f0cbc6fa7db59d
Kyle Cesare <kcesare@gmail.com>
2015-01-08 12:39:29 -0800

    Change main initialization order.

--- src/main.cpp
+++ src/main.cpp
@@ -6,24 +6,41 @@
 #include "variant/platform.hpp"
 #include "variant/unit.hpp"
 
-// static platform::HeartbeatThread heartbeatThread;
-static CommunicationThread<255> communicationThread(
-    reinterpret_cast<chibios_rt::BaseSequentialStreamInterface&>(SD1));
+class HeartbeatThread : public chibios_rt::BaseStaticThread<64> {
+public:
+  HeartbeatThread() : chibios_rt::BaseStaticThread<64>() {
+  }
+
+  virtual msg_t main() {
+    while(true) {
+      // palSetPad(GPIOE, GPIOE_LED3_RED);
+      sleep(MS2ST(500));
+      // palClearPad(GPIOE, GPIOE_LED3_RED);
+      sleep(MS2ST(500));
+    }
+    return 0;
+  }
+};
+
 
 int main(void) {
   halInit();
   chibios_rt::System::init();
 
+  // Build and initialize the system
   Platform platform;
+  platform.init();
+
   Unit unit(platform);
+  unit.init();
 
   // Start the background threads
-  // heartbeatThread.start(LOWPRIO);
-  communicationThread.start(LOWPRIO);
+  static HeartbeatThread heartbeatThread;
+  static CommunicationThread communicationThread(unit,
+      reinterpret_cast<chibios_rt::BaseSequentialStreamInterface&>(SD1));
 
-  // Build and initialize the system
-  platform.init();
-  unit.init();
+  heartbeatThread.start(LOWPRIO);
+  communicationThread.start(LOWPRIO);
 
   // Loop at a fixed rate forever
   // NOTE: If the deadline is ever missed then the loop will hang indefinitely.
