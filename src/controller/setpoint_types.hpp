fea137e74661a798ba75114095d0ffed0566086e
Kyle Cesare <kcesare@gmail.com>
2015-01-14 13:31:25 -0800

    Add stub position controller.

--- src/controller/setpoint_types.hpp
+++ src/controller/setpoint_types.hpp
@@ -1,6 +1,13 @@
 #ifndef SETPOINT_TYPES_HPP_
 #define SETPOINT_TYPES_HPP_
 
+struct position_setpoint_t {
+  float latitude_sp;
+  float longitude_sp;
+  float yaw_pos_sp;
+  float altitude_sp;
+};
+
 struct angular_position_setpoint_t {
   float roll_pos_sp;
   float pitch_pos_sp;
