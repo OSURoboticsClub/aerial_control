ab8e265c2326013683f46d5fc4079b76214bdda1
Kyle Cesare <kcesare@gmail.com>
2015-01-17 00:07:47 -0800

    Add gyro offsets for apollo.

--- variants/units/apollo/unit_config.hpp
+++ variants/units/apollo/unit_config.hpp
@@ -12,9 +12,9 @@ const float MAX_PITCH_ROLL_POS = 30.0;
 const float MAX_PITCH_ROLL_VEL = 100.0;
 
 // Sensor offsets
-const float GYR_X_OFFSET = 0.0;
-const float GYR_Y_OFFSET = 0.0;
-const float GYR_Z_OFFSET = 0.0;
+const float GYR_X_OFFSET = 0.05;
+const float GYR_Y_OFFSET = -0.05;
+const float GYR_Z_OFFSET = -0.01;
 const float ACC_X_OFFSET = 0.0;
 const float ACC_Y_OFFSET = 0.0;
 const float ACC_Z_OFFSET = 0.0;
