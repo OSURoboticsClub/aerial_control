ec2bdac470fb516e873e754482e3c0859190aa6a
Soo-Hyun Yoo <yoos117@gmail.com>
2015-01-17 20:45:25 -0800

    Add throttle constants to zeus and hera.

--- variants/units/zeus/unit_config.hpp
+++ variants/units/zeus/unit_config.hpp
@@ -1,3 +1,6 @@
+#ifndef UNIT_CONFIG_HPP_
+#define UNIT_CONFIG_HPP_
+
 namespace unit_config {
 
 const float DT = 0.001;
@@ -38,4 +41,16 @@ const float ANGPOS_Z_KP = 1.0;
 const float ANGPOS_Z_KI = 0.0;
 const float ANGPOS_Z_KD = 0.0;
 
+// A low throttle that will not spin the motors. Used when the propellers are
+// not spinning, but we do not want to send an invalid PWM signal.
+const float THROTTLE_SAFE = 0.30;
+
+// Minimum throttle to make the propellers spin.
+const float THROTTLE_MIN = 0.53;
+
+// Maximum allowable throttle.
+const float THROTTLE_MAX = 0.93;
+
 }
+
+#endif // UNIT_CONFIG_HPP_
