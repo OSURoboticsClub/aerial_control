780c6838a129f7e56349b3504d7d67ab5c9ce7d2
Soo-Hyun Yoo <yoos117@gmail.com>
2015-01-24 02:53:53 -0800

    Add aerial_v4 platform.

---
+++ variants/platforms/aerial_v4/pwm_platform.cpp
@@ -0,0 +1,29 @@
+#include "variant/pwm_platform.hpp"
+
+#include "hal.h"
+
+static const PWMConfig MOTOR_PWM_CONFIG {
+  500000,    // 500 kHz PWM clock frequency.
+  1000,      // PWM period 2.0 ms.
+  NULL,      // No callback.
+  {
+    {PWM_OUTPUT_ACTIVE_HIGH, NULL},
+    {PWM_OUTPUT_ACTIVE_HIGH, NULL},
+    {PWM_OUTPUT_ACTIVE_HIGH, NULL},
+    {PWM_OUTPUT_ACTIVE_HIGH, NULL}
+  },   // Channel configurations
+  0,0   // HW dependent
+};
+
+PWMPlatform::PWMPlatform() {
+  pwmStart(&PWMD8, &MOTOR_PWM_CONFIG);
+  palSetPadMode(GPIOC, 6, PAL_MODE_ALTERNATE(3));
+  palSetPadMode(GPIOC, 7, PAL_MODE_ALTERNATE(3));
+  palSetPadMode(GPIOC, 8, PAL_MODE_ALTERNATE(3));
+  palSetPadMode(GPIOC, 9, PAL_MODE_ALTERNATE(3));
+}
+
+void PWMPlatform::set(std::uint8_t ch, float dc) {
+  pwmcnt_t width = PWM_PERCENTAGE_TO_WIDTH(&PWMD8, dc * 10000.0f);
+  pwmEnableChannel(&PWMD8, ch, width);
+}
