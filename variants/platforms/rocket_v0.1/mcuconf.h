04d8fb11f7535a81e669bc0345cf5e11f42bde29
Soo-Hyun Yoo <yoos117@gmail.com>
2015-01-29 03:04:34 -0800

    Copy aerial v3 platform stuff into rocket v0.1.
    ..since everything's changed anyway.

--- variants/platforms/rocket_v0.1/mcuconf.h
+++ variants/platforms/rocket_v0.1/mcuconf.h
@@ -190,10 +190,10 @@
 #define STM32_PWM_USE_ADVANCED              FALSE
 #define STM32_PWM_USE_TIM1                  TRUE
 #define STM32_PWM_USE_TIM2                  FALSE
-#define STM32_PWM_USE_TIM3                  FALSE
+#define STM32_PWM_USE_TIM3                  TRUE
 #define STM32_PWM_USE_TIM4                  FALSE
 #define STM32_PWM_USE_TIM5                  FALSE
-#define STM32_PWM_USE_TIM8                  FALSE
+#define STM32_PWM_USE_TIM8                  TRUE
 #define STM32_PWM_USE_TIM9                  FALSE
 #define STM32_PWM_TIM1_IRQ_PRIORITY         7
 #define STM32_PWM_TIM2_IRQ_PRIORITY         7
@@ -208,7 +208,7 @@
  */
 #define STM32_SERIAL_USE_USART1             TRUE
 #define STM32_SERIAL_USE_USART2             FALSE
-#define STM32_SERIAL_USE_USART3             FALSE
+#define STM32_SERIAL_USE_USART3             TRUE
 #define STM32_SERIAL_USE_UART4              FALSE
 #define STM32_SERIAL_USE_UART5              FALSE
 #define STM32_SERIAL_USE_USART6             FALSE
--- variants/platforms/rocket_v0.1/spi_platform.cpp
+++ variants/platforms/rocket_v0.1/spi_platform.cpp
@@ -3,6 +3,9 @@
 #include "hal.h"
 
 SPIPlatform::SPIPlatform() {
+  palSetPadMode(GPIOB, 3, PAL_MODE_ALTERNATE(5) | PAL_STM32_OSPEED_HIGHEST); // SPI1 SCK
+  palSetPadMode(GPIOB, 4, PAL_MODE_ALTERNATE(5) | PAL_STM32_OSPEED_HIGHEST); // SPI1 MISO
+  palSetPadMode(GPIOB, 5, PAL_MODE_ALTERNATE(5) | PAL_STM32_OSPEED_HIGHEST); // SPI1 MOSI
   palSetPadMode(GPIOB, 2, PAL_MODE_OUTPUT_PUSHPULL | PAL_STM32_OSPEED_HIGHEST); // MPU-6000 CS
-  palSetPad(GPIOB, 2); //Unselect
+  palSetPad(GPIOB, 2); // Unselect
 }
