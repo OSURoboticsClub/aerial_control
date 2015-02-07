533aff3604eb94ee2dbdccd2c7213e0fff3e4774
Soo-Hyun Yoo <yoos117@gmail.com>
2015-02-07 08:35:35 -0800

    refs #7 Set helios GPS UART baudrate to 9600.

--- variants/platforms/aerial_v4/usart_platform.cpp
+++ variants/platforms/aerial_v4/usart_platform.cpp
@@ -20,7 +20,7 @@ static const SerialConfig UART4_CONFIG {
 
 // UART6 configuration (GPS)
 static const SerialConfig UART6_CONFIG {
-  38400,
+  9600,
   0,
   USART_CR2_STOP1_BITS | USART_CR2_LINEN,
   0
