88fae25bbdddcd6a2e63e449b886176809a8b775
Soo-Hyun Yoo <yoos117@gmail.com>
2015-03-20 11:14:31 -0700

    Add rocket unit files.

---
+++ variants/units/celestep/unit_data.hpp
@@ -0,0 +1,54 @@
+#ifndef UNIT_DATA_HPP_
+#define UNIT_DATA_HPP_
+
+#include "communication/communicator.hpp"
+#include "estimator/world_estimator.hpp"
+#include "estimator/atmospheric_location_estimator.hpp"
+#include "estimator/dcm_attitude_estimator.hpp"
+#include "motor/esra_payload_motor_mapper.hpp"
+#include "input/offboard_input_source.hpp"
+#include "sensor/sensor_measurements.hpp"
+#include "system/payload_system.hpp"
+#include "util/optional.hpp"
+#include "variant/platform.hpp"
+
+static const float MOTOR_PWM_MIN = 0.53f;
+static const float MOTOR_PWM_MAX = 0.93f;
+static const float MOTOR_PWM_SAFE = 0.30f;
+
+struct UnitData {
+  PWMDeviceGroup<1> motors;
+  EsraPayloadMotorMapper motorMapper;
+
+  WorldEstimator world;
+  AtmosphericLocationEstimator location;
+  DCMAttitudeEstimator attitude;
+  OffboardInputSource inputSource;
+
+  PayloadSystem system;
+
+  UnitData(Platform& platform, Communicator& communicator)
+    : motors(platform.get<PWMPlatform>(),
+        { 0 },                                       // channels
+        { 0.0f },                                    // offsets
+        0.0f, 1.0f,                                  // input range
+        MOTOR_PWM_MIN, MOTOR_PWM_MAX, MOTOR_PWM_SAFE // output range
+      ),
+      motorMapper(motors, communicator),
+      location(communicator),
+      attitude(communicator),
+      world(location, attitude, communicator),
+      inputSource(communicator),
+      system(
+          platform.getIdx<Accelerometer>(0),
+          std::experimental::make_optional(&platform.getIdx<Accelerometer>(1)),
+          std::experimental::make_optional(&platform.get<Barometer>()),
+          std::experimental::make_optional(&platform.get<GPS>()),
+          platform.get<Gyroscope>(),
+          std::experimental::make_optional(&platform.get<Magnetometer>()),
+          world, inputSource, motorMapper, communicator,
+          platform) {
+  }
+};
+
+#endif
