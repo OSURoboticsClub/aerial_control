b84ae8804fdfb66e8a0d5fd21afeedf7e9119017
Soo-Hyun Yoo <yoos117@gmail.com>
2015-04-22 03:11:42 -0700

    Slow down UART4 baudrate.

--- variants/platforms/rocket_v0.1/usart_platform.cpp
+++ variants/platforms/rocket_v0.1/usart_platform.cpp
@@ -12,7 +12,7 @@ static const SerialConfig UART2_CONFIG {
 
 // UART4 configuration (Unused)
 static const SerialConfig UART4_CONFIG {
-  460800,
+  115200,
   0,
   USART_CR2_STOP1_BITS | USART_CR2_LINEN,
   0
