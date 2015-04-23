a70b659f5a58de5f7ea50d646db22ce07dc51273
Soo-Hyun Yoo <yoos117@gmail.com>
2015-04-23 03:29:21 -0700

    refs #18 Update FS writer thread to work with new functions.

--- src/filesystem/fs_writer_thread.cpp
+++ src/filesystem/fs_writer_thread.cpp
@@ -10,14 +10,14 @@ msg_t FsWriterThread::main() {
   BaseSequentialStream *chp = (BaseSequentialStream*)&SD4;
   chprintf(chp, "Filesystem thread online.\r\n");
 
-  if (fs.connect()) {
-    fs.mount();
-  }
+  while (!fs.connect()) chThdSleepMilliseconds(10);
+  while (!fs.mount())   chThdSleepMicroseconds(10);
+  while (!fs.openNew()) chThdSleepMilliseconds(10);
 
   while(true) {
     // Check if there is data in the buffer that has not yet been written.
     while(bottom != top) {
-      fs.write(buffer[bottom++]);
+      fs.write(&buffer[bottom++], 1);
 
       // Wrap if the end of the buffer is reached.
       if(bottom >= buffer.size()) {
@@ -26,7 +26,7 @@ msg_t FsWriterThread::main() {
     }
 
     // TODO(kyle): Just yield, or sleep?
-    chThdSleepMicroseconds(10000);   // TODO(yoos): yield in this thread makes things not work.
+    chThdSleepMicroseconds(10);   // TODO(yoos): yield in this thread makes things not work.
   }
 
   // Should never get here
