6c691b89e65512aeb21d55c550b84f30832d965d
Soo-Hyun Yoo <yoos117@gmail.com>
2015-05-10 23:19:54 -0700

    refs #44 Finish implementing H3LIS331DL driver.
    Configured for 100g FS range.

--- src/drivers/h3lis331dl.cpp
+++ src/drivers/h3lis331dl.cpp
@@ -9,7 +9,7 @@ void H3LIS331DL::init() {
   txbuf[1] = 0b00111111;   // PM: normal mode, DR: 1000Hz, XYZ: enabled
   txbuf[2] = 0b00000000;   // Defaults
   txbuf[3] = 0b00000000;   // Defaults
-  txbuf[4] = 0b11110000;   // BDU: on, BLE: data MSB at lower addr, FS: 400g
+  txbuf[4] = 0b11000000;   // BDU: on, BLE: data MSB at lower addr, FS: 100g
   txbuf[5] = 0b00000011;   // Turned on
   exchange(6);
 }
@@ -27,15 +27,16 @@ AccelerometerReading H3LIS331DL::readAccel() {
   txbuf[0] = h3lis331dl::OUT_X_L | (1<<7) | (1<<6);   // Auto-increment read address
   exchange(7);
 
-  reading.axes[0] = ((int16_t) ((rxbuf[1]<<8) | rxbuf[2])) * 400.0 / 32768 + accOffsets[0];
-  reading.axes[1] = ((int16_t) ((rxbuf[3]<<8) | rxbuf[4])) * 400.0 / 32768 + accOffsets[1];
-  reading.axes[2] = ((int16_t) ((rxbuf[5]<<8) | rxbuf[6])) * 400.0 / 32768 + accOffsets[2];
+  reading.axes[0] = ((int16_t) ((rxbuf[1]<<8) | rxbuf[2]))/16 * 0.049 + accOffsets[0];
+  reading.axes[1] = ((int16_t) ((rxbuf[3]<<8) | rxbuf[4]))/16 * 0.049 + accOffsets[1];
+  reading.axes[2] = ((int16_t) ((rxbuf[5]<<8) | rxbuf[6]))/16 * 0.049 + accOffsets[2];
 
-  BaseSequentialStream* chp = (BaseSequentialStream*)&SD4;
-  static int i=0;
-  if ((i++)%100 == 0) {
-    chprintf(chp, "H3: %6d %6d %6d\r\n", reading.axes[0], reading.axes[1], reading.axes[2]);
-  }
+  // DEBUG
+  //BaseSequentialStream* chp = (BaseSequentialStream*)&SD4;
+  //static int i=0;
+  //if ((i++)%10 == 0) {
+  //  chprintf(chp, "H3: %8f %8f %8f\r\n", reading.axes[0], reading.axes[1], reading.axes[2]);
+  //}
 
   return reading;
 }
@@ -44,12 +45,6 @@ bool H3LIS331DL::healthy() {
   txbuf[0] = h3lis331dl::WHO_AM_I | (1<<7);
   txbuf[1] = 0x00;
   exchange(2);
-
-  BaseSequentialStream* chp = (BaseSequentialStream*)&SD4;
-  static int i=0;
-  if ((i++)%100 == 0) {
-    chprintf(chp, "H3 whoami: %x\r\n", rxbuf[1]);
-  }
 
   if (rxbuf[1] != 0x32) {
     return false;
