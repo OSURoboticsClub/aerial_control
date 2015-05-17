0915313aa227aa8ec7c42a0eb312c2ffc41c47b1
Soo-Hyun Yoo <yoos117@gmail.com>
2015-05-17 03:14:58 -0700

    Ignore arm status for canards.

--- src/motor/esra_rocket_motor_mapper.cpp
+++ src/motor/esra_rocket_motor_mapper.cpp
@@ -13,5 +13,5 @@ EsraRocketMotorMapper::EsraRocketMotorMapper(PWMDeviceGroup<1>& motors, Communic
 void EsraRocketMotorMapper::run(bool armed, ActuatorSetpoint& input) {
   // Interpret input roll as servo angle
   std::array<float, 1> outputs { input.roll };
-  motors.set(armed, outputs);
+  motors.set(true, outputs);   // Ignored armed setting for relatively safe servos. TODO(yoos): Obviously a hack. Fix.
 }
