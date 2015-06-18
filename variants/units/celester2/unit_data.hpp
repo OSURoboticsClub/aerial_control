4f5d1eee22d71c9c1a8c94d02ec47cb46e080cd5
Soo-Hyun Yoo <yoos117@gmail.com>
2015-06-18 06:09:34 -0700

    Add celester2.

---
+++ variants/units/celester2/unit_data.hpp
@@ -0,0 +1,56 @@
+#ifndef UNIT_DATA_HPP_
+#define UNIT_DATA_HPP_
+
+#include "communication/communicator.hpp"
+#include "estimator/world_estimator.hpp"
+#include "estimator/atmospheric_location_estimator.hpp"
+#include "estimator/dcm_attitude_estimator.hpp"
+#include "filesystem/logger.hpp"
+#include "input/offboard_input_source.hpp"
+#include "motor/esra_rocket_motor_mapper.hpp"
+#include "sensor/sensor_measurements.hpp"
+#include "system/rocket_system.hpp"
+#include "util/optional.hpp"
+#include "variant/platform.hpp"
+
+static const float MOTOR_PWM_MIN = 0.53f;
+static const float MOTOR_PWM_MAX = 0.93f;
+static const float MOTOR_PWM_SAFE = 0.30f;
+
+struct UnitData {
+  PWMDeviceGroup<1> motors;
+  EsraRocketMotorMapper motorMapper;
+
+  WorldEstimator world;
+  AtmosphericLocationEstimator location;
+  DCMAttitudeEstimator attitude;
+  OffboardInputSource inputSource;
+
+  RocketSystem system;
+
+  UnitData(Platform& platform, Communicator& communicator, Logger& logger)
+    : motors(platform.get<PWMPlatform>(),
+        { 7 },                                       // channels
+        { 0.0f },                                    // offsets
+        0.0f, 1.0f,                                  // input range
+        MOTOR_PWM_MIN, MOTOR_PWM_MAX, MOTOR_PWM_SAFE // output range
+      ),
+      motorMapper(motors, communicator, logger),
+      location(communicator, logger),
+      attitude(communicator, logger),
+      world(location, attitude, communicator, logger),
+      inputSource(communicator),
+      system(
+          platform.getIdx<Accelerometer>(0),
+          std::experimental::make_optional(&platform.getIdx<Accelerometer>(1)),
+          std::experimental::make_optional(&platform.get<Barometer>()),
+          std::experimental::make_optional(&platform.get<Geiger>()),
+          std::experimental::make_optional(&platform.get<GPS>()),
+          platform.get<Gyroscope>(),
+          std::experimental::nullopt,
+          world, inputSource, motorMapper, communicator, logger,
+          platform) {
+  }
+};
+
+#endif
