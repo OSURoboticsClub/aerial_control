07b60e35794042dea76610a034c38fb8191a3e30
Soo-Hyun Yoo <yoos117@gmail.com>
2015-07-20 00:13:04 -0700

    Add ICU platform to aerial_v4.

---
+++ variants/platforms/aerial_v4/icu_platform.cpp
@@ -0,0 +1,28 @@
+#include "variant/icu_platform.hpp"
+
+#include "hal.h"
+
+static void icuCallback(ICUDriver *icup) {
+  auto& platform = ICUPlatform::getInstance();
+  platform.trigger(icup);
+}
+
+static const ICUConfig ICU_CONFIG = {
+  ICU_INPUT_ACTIVE_HIGH,
+  200000,   // 200 kHz ICU clock frequency
+  NULL,
+  icuCallback,
+  NULL,
+  ICU_CHANNEL_1,
+  0
+};
+
+void ICUPlatform::init() {
+  icuInit();
+  icuStart(&ICUD2, &ICU_CONFIG);
+  palSetPadMode(GPIOA, 5, PAL_MODE_INPUT_PULLDOWN | PAL_MODE_ALTERNATE(1));
+  icuEnable(&ICUD2);
+}
+
+ICUPlatform::ICUPlatform() {
+}
