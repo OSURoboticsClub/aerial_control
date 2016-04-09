3e087f38de9a71edb22e8866ae662fe80f5b6a21
Kyle Cesare <kcesare@gmail.com>
2016-04-09 16:42:49 -0700

    Stub _verbose_terminate_handler to make binary smaller.

---
+++ src/stubs.cpp
@@ -0,0 +1,31 @@
+/**
+ * These stubs are necessary to make newlib work.
+ *
+ * See
+ * http://stackoverflow.com/questions/9632595/compiling-basic-c-file-for-the-arm-processor
+ */
+
+extern "C" {
+  void __cxa_pure_virtual() {while(1);}
+  void _sbrk() {while(1);}
+  void _exit(int e) {while(1);}
+  void _kill() {while(1);}
+  void _getpid() {while(1);}
+  void _write() {while(1);}
+  void _close() {while(1);}
+  void _fstat() {while(1);}
+  void _isatty() {while(1);}
+  void _lseek() {while(1);}
+  void _read() {while(1);}
+}
+
+// Per GCC documentation, this should be a unique value for every shared object.
+// We don't have to worry about dynamic linking since this is a main program, so
+// we can just zero it.
+void *__dso_handle = 0;
+
+namespace __gnu_cxx {
+  void __verbose_terminate_handler() {
+    for(;;);
+  }
+}
