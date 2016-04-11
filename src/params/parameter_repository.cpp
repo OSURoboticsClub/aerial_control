4c33c89fd5debef4545ac9f8f4059841f89a5b97
Kyle Cesare <kcesare@gmail.com>
2016-04-11 00:14:58 -0700

    Override new/delete to use ChibiOS allocator.
    This allows us to the the STL with dynamic allocation.

--- src/params/parameter_repository.cpp
+++ src/params/parameter_repository.cpp
@@ -1,35 +1,17 @@
 #include "params/parameter_repository.hpp"
 
 void ParameterRepository::def(char const *name, const float value) {
-  // If a value already exists for this key, don't overwrite it with a default.
-  for(std::size_t i = 0; i < parameterIdx; i++) {
-    if(parameters[i].first == name) {
-      return;
-    }
+  // Only write if there isn't an existing value.
+  if(parameters.count(name) == 0) {
+    parameters[name] = value;
   }
-
-  // Otherwise, push it onto the end of the parameter list.
-  parameters[parameterIdx++] = std::make_pair(name, value);
 }
 
 void ParameterRepository::set(char const *name, const float value) {
-  for(std::size_t i = 0; i < parameterIdx; i++) {
-    if(parameters[i].first == name) {
-      parameters[i] = std::make_pair(name, value);
-      break;
-    }
-  }
-
-  parameters[parameterIdx++] = std::make_pair(name, value);
+  // Overwrite any existing value.
+  parameters[name] = value;
 }
 
 float ParameterRepository::get(char const *name) const {
-  for(std::size_t i = 0; i < parameterIdx; i++) {
-    if(parameters[i].first == name) {
-      return parameters[i].second;
-    }
-  }
-
-  // TODO: Error?
-  return 0.0;
+  return parameters.at(name);
 }
