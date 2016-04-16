9aa1b4059ca5ea5dfe82335697385e527a047b67
Kyle Cesare <kcesare@gmail.com>
2016-04-16 00:07:40 -0700

    Sensors documentation.

--- src/sensor/sensors.hpp
+++ src/sensor/sensors.hpp
@@ -9,6 +9,13 @@
 #include "sensor/sensor_measurements.hpp"
 #include "util/optional.hpp"
 
+/**
+ * An aggregation of a number of available sensor sources.
+ *
+ * Sensors are held in optional types to designate whether or not the given
+ * sensor is availble. If a sensor is not available, then null optional types
+ * will be returned from the relevant methods.
+ */
 class Sensors {
 public:
   Sensors(optional<Accelerometer *> accelerometer,
@@ -19,10 +26,27 @@ public:
           optional<Magnetometer *> magnetometer);
 
   SensorMeasurements readAvailableSensors();
+
+  /**
+   * Perform a calibration step on each available sensor.
+   *
+   * @see Sensor#calibrateStep()
+   */
   void calibrateStep();
 
-  bool healthy();
+  /**
+   * Returns whether or not all available sensors are calibrated.
+   *
+   * @see Sensor#calibrate()
+   */
   bool calibrated();
+
+  /**
+   * Returns whether or not all available sensors are healthy.
+   *
+   * @see Sensor#healthy()
+   */
+  bool healthy();
 
 private:
   optional<Gyroscope *> gyroscope;
